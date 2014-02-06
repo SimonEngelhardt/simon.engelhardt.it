e4c2980f49d54bd920950ca10b3fdf6540329e1b
Simon Engelhardt <simon@engelhardt.it>
2014-02-06 15:42:27 -0500

    Added layout and index page
    So far only contains a header with profile pic and social links

--- source/javascripts/app.js
+++ source/javascripts/app.js
@@ -1,1 +1,7 @@
 $(document).foundation();
+
+// Override LinkedIn plugin icon style (see also CSS)
+IN.Event.on(IN, 'systemReady', function() {
+    $('.li-connect-mark')
+        .addClass('fi-social-linkedin');
+});
