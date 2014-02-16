9141503fa03e53ab278a8ebbad8f96220cccb8da
Simon Engelhardt <simon@engelhardt.it>
2014-02-15 21:02:16 -0500

    First WIP stab at experiences

--- source/javascripts/app.js
+++ source/javascripts/app.js
@@ -8,6 +8,24 @@ IN.Event.on(IN, 'systemReady', function() {
 
 var resumeApp = angular.module('resumeApp', []);
 
+resumeApp.controller('ExperienceCtrl', ['$scope', 'sheets', '$log', function ($scope, sheets, $log) {
+  sheets.getExperiences().then(function(experiences) {
+    var dateFormat = 'MM/DD/YYYY'; // Date format from Google Sheets API
+    angular.forEach(experiences, function(experience) {
+      if (experience.start) experience.start = moment(experience.start, dateFormat);
+      if (experience.end) {
+        experience.end = moment(experience.end, dateFormat);
+      }
+      else {
+        experience.end = moment();
+        experience.current = true;
+      }
+      if (experience.start && experience.end) experience.duration = moment.duration(experience.end - experience.start);
+    });
+    $scope.experiences = experiences;
+  });
+}]);
+
 resumeApp.controller('ProjectsCtrl', ['$scope', 'sheets', '$log', function ($scope, sheets, $log) {
   $scope.roles = [];
   var unfilteredRole = 'All';
@@ -21,6 +39,7 @@ resumeApp.controller('ProjectsCtrl', ['$scope', 'sheets', '$log', function ($sco
   sheets.getProjects().then(function(projects){
     $scope.projects = projects;
     angular.forEach($scope.projects, function(project){
+      project.extendedDescriptionVisible = false;
       angular.forEach(project.roles, function(role){
         if ($scope.roles.indexOf(role) === -1)
           $scope.roles.push(role);
@@ -41,35 +60,44 @@ resumeApp.controller('ProjectsCtrl', ['$scope', 'sheets', '$log', function ($sco
 
 resumeApp.factory('sheets', ['$http', '$log', function($http, $log){
   var sheetUrls = {
-        projects: 'https://spreadsheets.google.com/feeds/list/0ApJXKMOVLglTdE04c2Y0N192VWJQSlVzTWpicDBqbEE/1/public/values?alt=json'
+        projects:   'https://spreadsheets.google.com/feeds/list/0ApJXKMOVLglTdE04c2Y0N192VWJQSlVzTWpicDBqbEE/1/public/values?alt=json',
+        experience: 'https://spreadsheets.google.com/feeds/list/0ApJXKMOVLglTdE04c2Y0N192VWJQSlVzTWpicDBqbEE/3/public/values?alt=json'
       },
       keyPrefix = 'gsx$',
       valuePropertyName = '$t';
 
+  // Simple mapping of all properties from Google spreadsheet columns (in format such as entry.gsx$columnname.$t)
+  function mapSheet(data) {
+    var objects = data.feed.entry.map(function(entry) {
+      var object = {};
+      
+      for (key in entry) {
+        if (key.indexOf(keyPrefix) === 0) {
+          var value = entry[key][valuePropertyName];
+
+          if (value.indexOf('\n') !== -1)
+            value = value.split('\n');
+          else
+            value = value.toString();
+
+          object[key.substring(keyPrefix.length)] = value;
+        }
+      }
+      return object;
+    });
+
+    return objects;
+  }
+
   return {
     getProjects: function() {
       return $http.get(sheetUrls.projects).then(function(response) {
-        var projects = response.data.feed.entry.map(function(entry) {
-          // Simple mapping of all properties from Google spreadsheet columns (in format such as entry.gsx$columnname.$t)
-          var project = {
-            extendedDescriptionVisible: false
-          };
-          
-          for (key in entry) {
-            if (key.indexOf(keyPrefix) === 0) {
-              var value = entry[key][valuePropertyName];
-
-              if (value.indexOf('\n') !== -1)
-                value = value.split('\n');
-              else
-                value = value.toString();
-
-              project[key.substring(keyPrefix.length)] = value;
-            }
-          }
-          return project;
-        });
-        return projects;
+        return mapSheet(response.data);
+      });
+    },
+    getExperiences: function() {
+      return $http.get(sheetUrls.experience).then(function(response) {
+        return mapSheet(response.data);
       });
     }
   };
